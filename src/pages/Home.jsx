9fcc6b6ca41469d60802391322619fc812307955
RenzoFantu <rfantuzzi@gmail.com>
2024-07-11 23:42:27 -0400

    cambie ruta boton principal home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className="text-container">
             <h1>Bienvenido a  BARBA!!!🐵</h1>
             <h3>¡Descubre Tu Mejor Versión! Transforma tu Estilo, Eleva tu Confianza</h3>
-            <Button as ={Link} to = '/products' className = 'head-button' >Para tu Barba</Button>
+            <Button as ={Link} to = '/allproducts' className = 'head-button' >Para tu Barba</Button>
         </div>
       </div>
       <h4 className='titlle-h4'>Estás frente al espejo, listo para darle a tu barba el cuidado que se merece. Pero, ¿qué es lo que realmente necesitas para lograr una barba verdaderamente épica? La respuesta es simple: ¡Epic Barba!</h4>
